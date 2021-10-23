8f513abebd03b9d75d30d47c776d7071da40bd55
GREESHMABS <89036513+GREESHMABS@users.noreply.github.com>
2021-10-23 07:57:02 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const multer = require('multer');
 const adminRouter = express.Router();
+const Bookdata = require('../model/Bookdata');
 function router(nav){
     adminRouter.get('/',function(req,res){
         res.render('addbooks',{
@@ -7,9 +9,22 @@ function router(nav){
             title:'Library App'
         });
     }); 
-    adminRouter.get('/add',function(req,res){
-        res.send("New Book is Added!!");
-    })
+    adminRouter.post('/add',function(req,res){
+        var item = {
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            language: req.body.language,
+            Year: req.body.Year,
+            page: req.body.page,
+            date: req.body.date,
+            details:req.body.details,
+            image: req.file.path
+        }
+       var book = Bookdata(item);
+       book.save();
+       res.redirect('/books');
+    });
     return adminRouter;
 }
 module.exports = router;
